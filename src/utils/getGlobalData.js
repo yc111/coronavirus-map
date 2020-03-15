40f5ab7930f05bb9e2b5bf0511df885c7f83cac2
yc111 <yinchuan_111@163.com>
2020-03-15 20:36:46 +0800

    fix early date counter bug

--- src/utils/getGlobalData.js
+++ src/utils/getGlobalData.js
@@ -1,4 +1,12 @@
 
+function cleanData(data) {
+  return {
+    confirmedNum: data.confirmedNum || 0,
+    deathsNum: data.deathsNum || 0,
+    curesNum: data.curesNum || 0,
+  };
+}
+
 export function getGlobalData(data) {
   const { world, nationTotal } = data;
   const globalTotal = {
@@ -6,7 +14,7 @@ export function getGlobalData(data) {
     death: 0,
     cured: 0,
   };
-  const list = [];
+  let list = [];
   const chinaData = {
     name: '中国',
     confirmedNum: nationTotal.confirmedTotal,
@@ -22,10 +30,12 @@ export function getGlobalData(data) {
   list.push(...newWorld);
   list.push(chinaData);
   list.sort((a, b) => b.confirmedNum - a.confirmedNum);
+  list = list.filter(item => item.confirmedNum || item.deathsNum || item.curesNum);
   list.forEach((item) => {
-    globalTotal.confirmed += item.confirmedNum;
-    globalTotal.death += item.deathsNum;
-    globalTotal.cured += item.curesNum;
+    const temp = cleanData(item);
+    globalTotal.confirmed += temp.confirmedNum;
+    globalTotal.death += temp.deathsNum;
+    globalTotal.cured += temp.curesNum;
   });
   return { globalTotal, list };
 }
